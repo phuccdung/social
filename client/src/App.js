24330ae94e7c4f7ab0935a4439b1002be7fbd30d
phuccdung <18521258@gm.uit.edu.vn>
2022-07-25 15:50:27 +0700

    router page

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,27 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import React from "react";
+import {
+  BrowserRouter ,
+  Routes,
+  Route
+
+} from "react-router-dom";
+
 
 function App() {
-  return <Profile/>;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+      <Route path="/profile/:username" element={<Profile/>}></Route>
+
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
